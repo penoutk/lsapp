1cdafc163a0719d4cc2bd8020c45a54e7e3034ad
penoutk <peno.u.tk@outlook.com>
2020-05-24 13:59:53 +0300

    [IMPLEMENT] blade template layouts [check description]
    - [ADD] app.blade.php in views.layouts for boilerplate code for all pages
    - [REMOVE] boilerplate code for all pages and implement balde template

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-        <h1>Welcome to Laravel!</h1>
-        <p>This is the laravel application from the "Laravel From Scratch" Youtube series.</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome to Laravel!</h1>
+    <p>This is the laravel application from the "Laravel From Scratch" Youtube series.</p>
+@endsection
