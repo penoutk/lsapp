ef05393c6bea991514aa1bbb202111692fb20103
penoutk <peno.u.tk@outlook.com>
2020-05-25 22:03:28 +0300

    [IMPLEMENT] navbar [check description]
    - [ADD] navbar in resources/views/inc and include in app layout
    - (inc is short for includes, which will contain files to be included)
    - [ADD] bootstrap classes to app and services layouts
    - [ADD] jumbotron and buttons to index layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
