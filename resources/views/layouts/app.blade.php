82eb8ef6e51097f9c595478d82354306aa65a9c2
penoutk <peno.u.tk@outlook.com>
2020-05-25 21:36:33 +0300

    [IMPLEMENT] compiling assets [check description]
    - [ADD] _custom, _variables and app .scss files in resources/sass
    - [ADD] required dependencies to package.json
    - [ADD] node modules using npm install
    - compile assets using npm run dev
    - compiling adds a few assets to the public folder and package-lock.json
    - [ADD] app.css styles asset to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name', 'LSAPP')}}</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         @yield('content')
